1660fa30d0bb8c65284b6f494ada67f481b6b7f8
Anthony Scinocco <scinocco.a@gmail.com>
2017-01-02 22:50:41 -0500

    added owner to mass assignable

--- event-manager/app/Event.php
+++ event-manager/app/Event.php
@@ -14,7 +14,7 @@ class Event extends Model
      */
     protected $fillable = [
         'title', 'description', 'location',
-        'attire', 'date'
+        'attire', 'date', 'owner',
     ];
 
     /**
