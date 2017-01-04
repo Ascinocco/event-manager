dd85e24565e059f4cde396c5202985cc2f0782e6
Anthony Scinocco <scinocco.a@gmail.com>
2017-01-03 19:18:21 -0500

    finally got view working with homestead
    Now I need to rip out docker and plugin vagrant. Woot.

---
+++ event-manager/resources/assets/js/components/UserDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>User Dashboard</h1>
+        <p>Eat shit vue</p>
+        blah
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Dashboard Component ready.')
+        }
+    }
+</script>
